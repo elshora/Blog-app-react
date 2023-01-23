f5c18e87f6a10c5a00f6e66a5396887f3d5efd8b
Mahmoud ELshora <mahmelshora@gmail.com>
2023-01-23 19:27:34 +0200

    Post crud operation

---
+++ api/routes/posts.js
@@ -0,0 +1,76 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const Post = require("../models/Post");
+
+// create post
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+// update Post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedPost);
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    } else {
+      res.status(401).json("not authorized to this action");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+//   delete
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        await post.delete();
+        res.status(200).json("Post Deleted");
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    } else {
+      res.status(401).json("not authorized to this action");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+// get Post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+// get all Posts
+
+router.get("/", async (_req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+module.exports = router;
