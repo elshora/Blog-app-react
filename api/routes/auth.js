5c4bfeab087ad54e29d1007f79ca00c60ca807c6
Mahmoud ELshora <mahmelshora@gmail.com>
2023-01-23 17:14:19 +0200

    login and register done

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,18 +1,36 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
 // Register
 router.post("/register", async (req, res) => {
-  console.log("hi");
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hashedPass,
     });
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (error) {
-    res.status(500).json(err);
+    res.status(500).json({ message: "username or email already used" });
+  }
+});
+
+// login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("Wrong Info");
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    if (!validated) res.status(400).json("Wrong Info");
+    const { password, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
   }
 });
 module.exports = router;
